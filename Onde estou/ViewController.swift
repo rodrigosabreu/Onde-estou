0df6527779d8ca8b342e7211251b98de6a593809
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-06 17:31:14 -0200

    layout

--- Onde estou/ViewController.swift
+++ Onde estou/ViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet var mapa: MKMapView!
+    
+    
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
