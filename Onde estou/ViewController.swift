58c948b475944e9d63be1ea497c10796add6ee59
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-07 13:39:29 -0200

    Configuração de alerta caso o usuário cancelar a permissão de localização

--- Onde estou/ViewController.swift
+++ Onde estou/ViewController.swift
@@ -9,11 +9,10 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet var mapa: MKMapView!
-    
-    
+    var gerenciadorLocalizacao = CLLocationManager()
     
     override var prefersStatusBarHidden: Bool{
         return true
@@ -21,9 +20,50 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.desiredAccuracy = kCLLocationAccuracyBest
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+        
     }
 
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status != .authorizedWhenInUse{
+            
+            let alertaController = UIAlertController(title: "Permissão de localização",
+                        message: "Necessário permissão para o acesso à sua localização! por favor habilite.",
+                        preferredStyle: .alert)
+            
+            let acaoConfiguracoes = UIAlertAction(title: "Abrir configurações", style: .default, handler: { (alertaConfiguracoes) in
+                
+                if let configuracoes = NSURL(string:UIApplicationOpenSettingsURLString){
+                    UIApplication.shared.open(configuracoes as URL)
+                }
+                
+            })
+            
+            let acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            
+            alertaController.addAction(acaoConfiguracoes)
+            alertaController.addAction(acaoCancelar)
+            
+            present(alertaController, animated: true, completion: nil)
+            
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
